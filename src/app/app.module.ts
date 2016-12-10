9c9846ea5d402d863f8187eda22b666f04ee972b
Michael Kaaden <michael@kaaden.net>
2016-12-10 18:04:49 +0100

    Reformat code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
