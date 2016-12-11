c9bef7beb13b7170cded6f3bf84a45e8d0b0273c
Michael Kaaden <michael@kaaden.net>
2016-12-11 14:43:10 +0100

    Introduce the book-list component (completing chapter 7.1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        BookListComponent
     ],
     imports: [
         BrowserModule,
