09d000b6af8a1ce13e87ee6352a4ea53d01fb1b1
Michael Kaaden <github@kaaden.net>
2016-12-13 22:45:24 +0100

    Move book array to the BookStoreService (completing chapter 8.1)

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Book, Thumbnail } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
     selector: 'bm-book-list',
@@ -10,29 +11,11 @@ export class BookListComponent implements OnInit {
     books: Book[];
     @Output() showDetailsEvent = new EventEmitter<Book>();
 
+    constructor(private bookStoreService: BookStoreService) {
+    }
+
     ngOnInit() {
-        this.books = [
-            new Book(
-                '9783864903571',
-                'Angular',
-                ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode'],
-                new Date(2016, 5, 26),
-                'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen',
-                5,
-                [new Thumbnail('https://angular-buch.com/angular-buch.jpg', 'Buchcover')],
-                'Dieses Buch vermittelt einen Schnelleinstieg in Angular  ...'
-            ),
-            new Book(
-                '9783864901546',
-                'AngularJS',
-                ['Philipp Tarasiewicz', 'Robin Böhm'],
-                new Date(2014, 5, 29),
-                'Eine praktische Einführung',
-                5,
-                [new Thumbnail('https://goo.gl/Y5lFVE', 'Buchcover')],
-                'Dieses Buch führt Sie anhand eines zusammenhängenden Beispielprojekts ...'
-            )
-        ];
+        this.books = this.bookStoreService.getAll();
     }
 
     showDetails(book): void {
---
+++ src/app/shared/book-store.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Book, Thumbnail } from './book';
+
+@Injectable()
+export class BookStoreService {
+    books: Book[];
+
+    constructor() {
+        this.books = [
+            new Book(
+                '9783864903571',
+                'Angular',
+                ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode'],
+                new Date(2016, 5, 26),
+                'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen',
+                5,
+                [new Thumbnail('https://angular-buch.com/angular-buch.jpg', 'Buchcover')],
+                'Dieses Buch vermittelt einen Schnelleinstieg in Angular  ...'
+            ),
+            new Book(
+                '9783864901546',
+                'AngularJS',
+                ['Philipp Tarasiewicz', 'Robin Böhm'],
+                new Date(2014, 5, 29),
+                'Eine praktische Einführung',
+                5,
+                [new Thumbnail('https://goo.gl/Y5lFVE', 'Buchcover')],
+                'Dieses Buch führt Sie anhand eines zusammenhängenden Beispielprojekts ...'
+            )
+        ];
+    }
+
+    getAll(): Book[] {
+        return this.books;
+    }
+}
