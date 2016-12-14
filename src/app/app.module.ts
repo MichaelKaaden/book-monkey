24709ae92701d53acb36e5a02b6e852a7dd80c2e
Michael Kaaden <michael@kaaden.net>
2016-12-14 22:44:33 +0100

    Implemented routing (completing chapter 8)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,20 @@ import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookStoreService } from './shared/book-store.service';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         BookListComponent,
         BookListItemComponent,
-        BookDetailsComponent
+        BookDetailsComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
+        AppRoutingModule,
         FormsModule,
         HttpModule
     ],
--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,14 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 
 import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'bm-book-details',
     templateUrl: './book-details.component.html'
 })
-export class BookDetailsComponent {
-    @Input() book: Book;
+export class BookDetailsComponent implements OnInit {
+    book: Book;
     @Output() showListEvent = new EventEmitter<any>();
+
+    constructor(private bookStoreService: BookStoreService,
+                private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        let params = this.route.snapshot.params;
+        this.book = this.bookStoreService.getSingle(params['isbn']);
+    }
 
     getRating(num: number) {
         return new Array(num);
