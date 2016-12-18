28119355441d7fd25ede623465468acff2c800d6
Michael Kaaden <michael@kaaden.net>
2016-12-18 22:47:21 +0100

    Add Template Driven Forms (completing chapter 10.2)

---
+++ src/app/book-form/book-form-error-messages.ts
@@ -0,0 +1,15 @@
+export class ErrorMessage {
+    constructor(public forControl: string,
+                public forValidator: string,
+                public text: string) {
+    }
+}
+
+export const BookFormErrorMessages = [
+    new ErrorMessage('title', 'required', 'Ein Buchtitel muss angegeben werden'),
+    new ErrorMessage('isbn', 'required', 'Es muss eine ISBN angegeben werden'),
+    new ErrorMessage('isbn', 'minlength', 'Die ISBN muss mindestens 10 Zeichen enthalten'),
+    new ErrorMessage('isbn', 'maxlength', 'Eine ISBN darf höchstens 13 Zeichen haben'),
+    new ErrorMessage('published', 'required', 'Es muss ein Erscheinungsdatum angegeben werden'),
+    new ErrorMessage('authors', 'required', 'Es muss ein Autor angegeben werden')
+];
