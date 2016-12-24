6198810f0b57f1866488ed3ded84e4088406b967
Michael Kaaden <github@kaaden.net>
2016-12-24 20:42:35 +0100

    Fix supposed useValueAsDate bug (thanks to Johannes Hoppe)

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -117,7 +117,7 @@ export class BookFormComponent implements OnInit {
             authors: this.buildAuthorsArray(),
             thumbnails: this.buildThumbnailsArray(),
             published: [
-                this.book.published
+                new Date(this.book.published)
             ]
         });
         this.myForm.valueChanges.subscribe(() => this.updateErrorMessages());
