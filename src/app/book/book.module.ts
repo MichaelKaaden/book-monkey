21273be2e79c6a6ff701e1ce40221921f2d8915d
Michael Kaaden <github@kaaden.net>
2016-12-29 18:21:45 +0100

    finish step 1: create modules

---
+++ src/app/book/book.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookRoutingModule } from './book-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BookRoutingModule
+  ],
+  declarations: []
+})
+export class BookModule { }
